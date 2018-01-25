13c54c2d1681fecdba2e39efdd6b333b585a59bd
GreenCord <steve@nevizen.com>
2018-01-24 19:22:03 -0500

    Basic express server

--- server.js
+++ server.js
@@ -12,10 +12,20 @@ var PORT = 3000;
 
 //Set up the server to handle data parsing 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
 app.use(bodyParser.json());
 
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.get('/table', function(req,res) {
+	res.sendFile(path.join(__dirname, 'table.html'));
+});
+
+app.get('/reserve', function(req,res){
+	res.sendFile(path.join(__dirname, 'reserve.html'));
+});
 
 
 
